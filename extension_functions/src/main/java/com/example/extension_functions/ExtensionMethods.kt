a080f8270c79e85e8fb96d3bb4d40986d95649cd
Zunnorain <zainshah787@hotmail.com>
2023-06-14 16:32:32 +0500

    Version 1

---
+++ extension_functions/src/main/java/com/example/extension_functions/ExtensionMethods.kt
@@ -0,0 +1,141 @@
+package com.example.extension_functions
+
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
+import android.os.Build
+import android.os.SystemClock
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
+import android.util.Log
+import android.util.Patterns
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+
+fun Context.showToastShort(msg:String){
+    Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+}
+fun Context.showToastLong(msg:String){
+    Toast.makeText(this,msg,Toast.LENGTH_LONG).show()
+}
+
+fun View.setSafeOnClickListener(debounceTime: Long = 1500L, action: () -> Unit) {
+    this.setOnClickListener(object : View.OnClickListener {
+        private var lastClickTime: Long = 0
+        override fun onClick(v: View) {
+            if (SystemClock.elapsedRealtime() - lastClickTime < debounceTime) return
+            else{
+                //Will always invoke else statement because system clock time is not the same since we have delayed by 1.5 seconds
+                action.invoke()
+                Log.d("TAG99", "onClick: Time not delayed Double CLick Occurred")
+
+            }
+            lastClickTime = SystemClock.elapsedRealtime()
+        }
+
+    })
+}
+
+fun String.isValidEmail() = !TextUtils.isEmpty(this) && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+
+fun <T> Context.openCustomActivity(it: Class<T>) {
+    val intent = Intent(this, it)
+    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+    startActivity(intent)
+}
+
+/**
+ *
+ * Usage
+   For example, In the MainActivity, how to call this?
+
+    if(isNetworkAvailable()){
+        //Perform your task
+    }else{
+        showToast("Internet is not available, Try Later")
+    }
+
+ *
+ */
+fun Context.isNetworkAvailable(): Boolean {
+    val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        val capabilities = manager.getNetworkCapabilities(manager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                return true
+            }
+        }
+    } else {
+        try {
+            val activeNetworkInfo = manager.activeNetworkInfo
+            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                return true
+            }
+        } catch (e: Exception) {
+            e.message
+        }
+    }
+    return false
+}
+
+fun EditText.onChange(textChanged: ((String) -> Unit)) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            textChanged.invoke(s.toString())
+        }
+    })
+}
+
+fun View.showSnackMessage(
+    message: String?,
+    anchorView: View? = null,
+    backgroundColor: Int,
+    textColor: Int,
+    length: Int = Snackbar.LENGTH_SHORT
+) {
+    message?.let {
+        try {
+            val snack = Snackbar.make(this, it, length)
+            snack.setBackgroundTint(ContextCompat.getColor(context, backgroundColor))
+            snack.setTextColor(ContextCompat.getColor(context, textColor))
+            snack.anchorView = anchorView
+            snack.show()
+        } catch (ex: Exception) {
+            ex.message
+        }
+    }
+}
+
+/**
+
+//Usage
+binding.loadingPb.hide()
+
+ */
+fun View.show() {
+    visibility = View.VISIBLE
+}
+fun View.hide() {
+    visibility = View.GONE
+}
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
+

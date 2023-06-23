d9079f8cf4f1d5745cef77f71eff59785c1f37de
Zunnorain <zainshah787@hotmail.com>
2023-06-23 10:48:08 +0500

    New extensions added

--- extension_functions/src/main/java/com/example/extension_functions/ExtensionMethods.kt
+++ extension_functions/src/main/java/com/example/extension_functions/ExtensionMethods.kt
@@ -1,7 +1,9 @@
 package com.example.extension_functions
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.net.ConnectivityManager
@@ -13,11 +15,17 @@ import android.text.TextUtils
 import android.text.TextWatcher
 import android.util.Log
 import android.util.Patterns
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 
 fun Context.showToastShort(msg:String){
@@ -148,5 +156,112 @@ fun viewToBitmap(binding: ConstraintLayout): Bitmap {
     val canvas = Canvas(myBitmap)
     view.draw(canvas)
     return myBitmap
+}
+
+fun View.showKeyboard() {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    this.requestFocus()
+    imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+}
+fun View.hideKeyboard() {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(windowToken, 0)
+}
+
+//Permissions
+fun <T : Activity> T.requestPermissionsFast(vararg permissions: String, REQUEST_CODE: Int) {
+    val permissionsToRequest = permissions.filter {
+        ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+    }
+
+    if (permissionsToRequest.isEmpty()) {
+        // All permissions are already granted
+        return
+    }
+
+    val shouldShowRationaleList = permissionsToRequest.map {
+        ActivityCompat.shouldShowRequestPermissionRationale(this, it)
+    }
+
+    if (shouldShowRationaleList.contains(true)) {
+        // Show the rationale dialog
+        showPermissionRationaleDialog(permissionsToRequest.toTypedArray(), REQUEST_CODE)
+    } else {
+        // Request the permissions directly
+        ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), REQUEST_CODE)
+    }
+}
+private fun <T : Activity> T.showPermissionRationaleDialog(permissions: Array<String>, REQUEST_CODE: Int) {
+    val builder = AlertDialog.Builder(this)
+        .setTitle("Permissions Required")
+        .setMessage("This app requires certain permissions to function properly.")
+        .setPositiveButton("Grant Permissions") { dialog, _ ->
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+            dialog.dismiss()
+        }
+        .setNegativeButton("Deny") { dialog, _ ->
+            dialog.dismiss()
+        }
+        .setCancelable(false)
+        .show()
+}
+
+//Alert Dialog
+fun Context.alert(
+    title: String? = null,
+    message: String? = null,
+    positiveButtonText: String,
+    positiveButtonAction: () -> Unit = {},
+    negativeButtonText: String? = null,
+    negativeButtonAction: () -> Unit = {},
+    neutralButtonText: String? = null,
+    neutralButtonAction: () -> Unit = {},
+) {
+    val builder = AlertDialog.Builder(this)
+    builder.setTitle(title)
+    builder.setMessage(message)
+    builder.setPositiveButton(positiveButtonText) { dialog, which ->
+        positiveButtonAction()
+    }
+    if (negativeButtonText != null) {
+        builder.setNegativeButton(negativeButtonText) { dialog, which ->
+            negativeButtonAction()
+        }
+    }
+    if (neutralButtonText != null) {
+        builder.setNeutralButton(neutralButtonText) { dialog, which ->
+            neutralButtonAction()
+        }
+    }
+    val dialog = builder.create()
+    dialog.show()
+}
+
+//RV
+fun <T> RecyclerView.setRecyclerViewData(
+    data: List<T>,
+    itemLayoutRes: Int,
+    bindItem: View.(T) -> Unit
+) {
+    adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): RecyclerView.ViewHolder {
+            val itemView = LayoutInflater.from(parent.context)
+                .inflate(itemLayoutRes, parent, false)
+            return object : RecyclerView.ViewHolder(itemView) {}
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val item = data[position]
+            holder.itemView.bindItem(item)
+            holder.itemView.setOnClickListener {
+                context.showToastShort("$item clicked")
+            }
+        }
+
+        override fun getItemCount(): Int = data.size
+    }
 }
 
